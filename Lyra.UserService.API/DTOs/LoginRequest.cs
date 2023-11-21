7f5434f7210f3c135631c5d9dc885c4cd80d6234
Jeffrin J <jjeffrin@gmail.com>
2023-11-21 08:56:07 +0530

    Delete Lyra.UserService.API directory

--- Lyra.UserService.API/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lyra.UserService.API.DTOs
-{
-    public class LoginRequest
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- Lyra.UserService.API/DTOs/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lyra.UserService.API.DTOs
-{
-    public class RegisterRequest
-    {
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
